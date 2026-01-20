e00e98d3cc1c0c220390672b6de9a056915c584e
astridev <abostridev@gmail.com>
2026-01-20 11:38:41 +0100

    notion de type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+type Props = {
+  username: string
+}
+
+function Welcome({ username }: Props) {
+  return (
+    <h1 className="text-3xl font-bold">
+      Welcome {username} 🚀
+    </h1>
+  )
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold">
-        🚀 Welcome to AstroChat
-      </h1>
+      <Welcome username="AstroUser" />
     </main>
   )
 }
